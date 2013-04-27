69bf96d0461e2abf98b03a11099200ed1ee6bc59
Mike Batanian <mikebatanian@gmail.com>
2013-04-27 16:47:49 -0400

    look at me, sending url

--- src/feedPocket.js
+++ src/feedPocket.js
@@ -1,20 +1,30 @@
 
-
-// $('.u0Entry').hover(function () {
-// $('.pageActionBar').hover(function () {
-// 	console.log('hello');
-// }, function () {
-// 	console.log('wrld');
-// });
-
 (function($) {
+
+	var sendToPocket = function (toAdd) {
+		$.ajax({
+			url: 'https://getpocket.com/v3/add',
+			type: 'POST',
+		 	contentType: 'application/json',
+		 	dataType: 'json',
+			data: JSON.stringify({
+		 		'url': toAdd,
+		 		'access_token': 'nan',
+				'consumer_key': 'nan'
+			}),
+			success: function () {
+			},
+			error: function (jqXhr, textStatus, errorThrown) {
+				alert('error saving to pocket');
+			}
+		});
+	};
 
 	var imgUrl = chrome.extension.getURL('lib/pocket.png');
 	var pocketElement = '<img class="pocket" data-page-entry-action="pocketEntry" title="Post to Pocket" src="'+imgUrl+'"">';
 
 	$(document).on('mouseenter', '.u0Entry', function () {
-		console.log('dowork');
-		console.log(pocketElement);
+
 		var ct = $(this).find('.condensedTools');
 		if (ct.find('.pocket').length === 0) {
 			ct.find('img[data-page-entry-action="facebookEntry"]').after(pocketElement);
@@ -23,7 +33,10 @@
 		
 	});
 
-		
+	$(document).on('click', '.pocket', function () {
+		var url = $(this).closest('.u0Entry').attr('data-alternate-link');
+		sendToPocket(url);
+	});
 
 })(jQuery);
 

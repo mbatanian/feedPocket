9e79541eaf507957d5f1804b2ee0b21c41e1c9c5
Mike Batanian <mikebatanian@gmail.com>
2013-04-27 16:18:22 -0400

    step 2: only add icon once

--- src/feedPocket.js
+++ src/feedPocket.js
@@ -10,17 +10,17 @@
 (function($) {
 
 	var imgUrl = chrome.extension.getURL('lib/pocket.png');
-	var pocketElement = '<img data-page-entry-action="pocketEntry" title="Post to Pocket" src="'+imgUrl+'"">';
+	var pocketElement = '<img class="pocket" data-page-entry-action="pocketEntry" title="Post to Pocket" src="'+imgUrl+'"">';
 
 	$(document).on('mouseenter', '.u0Entry', function () {
 		console.log('dowork');
 		console.log(pocketElement);
 		var ct = $(this).find('.condensedTools');
-		ct.find('img[data-page-entry-action="facebookEntry"]').after(pocketElement);
-		ct.find('img[data-page-entry-action="linkedInEntry"]').remove();
-		// ct.append(pocketElement);
-
-		// ct.find('img[data-page-entry-action="linkedInEntry"]').remove()
+		if (ct.find('.pocket').length === 0) {
+			ct.find('img[data-page-entry-action="facebookEntry"]').after(pocketElement);
+			ct.find('img[data-page-entry-action="bufferEntry"]').remove();
+		}
+		
 	});
 
 		

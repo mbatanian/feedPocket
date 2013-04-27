27d1c40d6b9734e2da827ce2a601eb6e92f50818
Mike Batanian <mikebatanian@gmail.com>
2013-04-27 17:30:55 -0400

    add shortcut key, clean up

--- src/feedPocket.js
+++ src/feedPocket.js
@@ -2,20 +2,21 @@
 
 (function($) {
 
-	var sendToPocket = function (toAdd, item) {
+	var sendToPocket = function (urlToAdd, condensedItem) {
+
 		$.ajax({
 			url: 'https://getpocket.com/v3/add',
 			type: 'POST',
 		 	contentType: 'application/json',
 		 	dataType: 'json',
 			data: JSON.stringify({
-		 		'url': toAdd,
+		 		'url': urlToAdd,
 		 		'access_token': 'nan',
-				'consumer_key': 'nan'
+		 		'consumer_key': 'nan'
 			}),
 			success: function () {
-				item.addClass('pocketed');
-				item.attr('src', chrome.extension.getURL('lib/pocketed.png'));
+				condensedItem.addClass('pocketed');
+				condensedItem.attr('src', chrome.extension.getURL('lib/pocketed.png'));
 			},
 			error: function (jqXhr, textStatus, errorThrown) {
 				alert('error saving to pocket');
@@ -23,24 +24,42 @@
 		});
 	};
 
-	var imgUrl = chrome.extension.getURL('lib/pocket.png');
-	var pocketElement = '<img class="pocket" data-page-entry-action="pocketEntry" title="Post to Pocket" src="'+imgUrl+'"">';
-
-	$(document).on('mouseenter', '.u0Entry', function () {
-
-		var ct = $(this).find('.condensedTools');
+	var addPocketDOMItem = function (item) {
+		console.log('writing');
+		var ct = item.find('.condensedTools');
 		if (ct.find('.pocket').length === 0) {
 			ct.find('img[data-page-entry-action="facebookEntry"]').after(pocketElement);
 			ct.find('img[data-page-entry-action="bufferEntry"]').remove();
 		}
-		
+	};
+
+	var scutKey = 66; // 'B'
+
+	var imgUrl = chrome.extension.getURL('lib/pocket.png');
+	var pocketElement = '<img class="pocket" data-page-entry-action="pocketEntry" title="Post to Pocket" src="'+imgUrl+'"">';
+
+	$(document).on('mouseenter', '.u0Entry', function () {
+		addPocketDOMItem($(this));
+	});
+
+	$(document).on('keyup', function (e) {
+		// find the selected entry...
+		var item = $('.selectedEntry')
+		// first, make sure the dom item exists...
+		addPocketDOMItem(item);
+		// then, if it's the proper key, send to pocket
+		if (e.which === scutKey) {
+			var pkitem = item.find('.pocket');
+			url = item.attr('data-alternate-link');
+			sendToPocket(url, pkitem);
+		}
 	});
 
 	$(document).on('click', '.pocket', function () {
 		var item = $(this);
 		if (!item.hasClass('pocketed')) {
-			var url = $(this).closest('.u0Entry').attr('data-alternate-link');
-			sendToPocket(url, $(this));
+			var url = item.closest('.u0Entry').attr('data-alternate-link');
+			sendToPocket(url, item);
 		}
 	});
 
